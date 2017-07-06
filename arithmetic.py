7c943ddd5d3b9dbce69abd57b4b63d1865cf0c8b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-06 12:05:35 -0700

    Added num1 ** num2 function

--- arithmetic.py
+++ arithmetic.py
@@ -35,7 +35,8 @@ def cube(num1):
 
 def power(num1, num2):
     """Raise num1 to the power of num and return the value."""
-
+    total = num1 ** num2
+    return total
 
 def mod(num1, num2):
     """Return the remainder of num / num2."""
