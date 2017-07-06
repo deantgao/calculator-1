3d292ad7b322cc7f3e0e8f893ce9b03b047368f7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-06 11:53:59 -0700

    Added multiplication function

--- arithmetic.py
+++ arithmetic.py
@@ -9,28 +9,30 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    total = num2 - num1
+    total = num1 - num2
     return total
 
-# def multiply(num1, num2):
-#     """Multiply the two inputs together."""
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    total = num1 * num2
+    return total
 
 
-# def divide(num1, num2):
-#     """Divide the first input by the second, returning a floating point."""
+def divide(num1, num2):
+    """Divide the first input by the second, returning a floating point."""
 
 
-# def square(num1):
-#     """Return the square of the input."""
+def square(num1):
+    """Return the square of the input."""
 
 
-# def cube(num1):
-#     """Return the cube of the input."""
+def cube(num1):
+    """Return the cube of the input."""
 
 
-# def power(num1, num2):
-#     """Raise num1 to the power of num and return the value."""
+def power(num1, num2):
+    """Raise num1 to the power of num and return the value."""
 
 
-# def mod(num1, num2):
-#     """Return the remainder of num / num2."""
+def mod(num1, num2):
+    """Return the remainder of num / num2."""
